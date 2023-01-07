80bab94bc6dca3c2f54d429180401eacc5b1417e
kkito0726 <k.kito0726@gmail.com>
2023-01-07 19:09:47 +0900

    🐛 周波数を求める処理で正しい値が出力されるように修正

--- analyze_chlamy.py
+++ analyze_chlamy.py
@@ -22,7 +22,7 @@ def read_data(spot_csv, track_csv, frame=601):
     return data
 
 def draw_moves(data, movie_height=841.59, threshold=230, dpi=300):
-    id = np.unique(data["TRACK_ID"].values)  
+    id = np.unique(data["TRACK_ID"].values)
     '''
     TRACK_IDが同じものごとにグループ化
     [
@@ -53,6 +53,38 @@ def draw_moves(data, movie_height=841.59, threshold=230, dpi=300):
             plt.ylim(0,1200)
     plt.show()
     
+def calc_frequencies(data, threshold=230, fps=600):
+    id = np.unique(data["TRACK_ID"].values)
+    
+    x_list = [data[data["TRACK_ID"] == i]["POSITION_X"].values for i in id]
+    y_list = [data[data["TRACK_ID"] == i]["POSITION_Y"].values for i in id]
+    
+    frequency_list = []
+    for x, y in zip(x_list, y_list):
+        x_diff, y_diff = np.diff(x), np.diff(y)
+        length = np.sqrt(x_diff**2 + y_diff**2).sum()
+        
+        if length > threshold:
+            N = len(x_diff) #サンプル数
+            dt = 1/fps #サンプリング周期(s) (今回の撮影条件は600 fps)
+            F = np.fft.fft(x_diff) # フーリエ変換
+            # フーリエ変換の結果を正規化
+            F = F / (N / 2)
+            freq = np.fft.fftfreq(N, d=dt) # 周波数スケール
+            Amp = np.abs(F) # 振幅スペクトル
+            stack = np.stack((freq[1:int(N/2)],Amp[1:int(N/2)])) 
+            
+            #振幅が最大値のindexを取得
+            max_indent = np.argmax(stack[1])
+            #振幅が最大値のindexの時の周波数を取得
+            max = stack[0][max_indent]
+            #listに加えていく
+            frequency_list.append(max)
+            
+    return frequency_list
+    
+    
+    
     
 if __name__ == '__main__':
     spot_csv = "./spot_1-601.csv"
@@ -61,3 +93,7 @@ if __name__ == '__main__':
     data = read_data(spot_csv, track_csv)
     
     draw_moves(data)
+    
+    frequency_list = calc_frequencies(data)
+    
+    print(frequency_list)
